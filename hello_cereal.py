35e3cbcbf18ee8e2065733f1259a4a6e62ff6b63
Ryuma Nakano <rjnl_85@hotmail.com>
2022-05-06 17:51:46 -0500

    Initial commit. Creating a simple op and job using cereal data

---
+++ hello_cereal.py
@@ -0,0 +1,17 @@
+import requests
+import csv
+
+from dagster import job, op, get_dagster_logger
+
+@op
+def hello_cereal():
+    response = requests.get("https://docs.dagster.io/assets/cereals.csv")
+    lines = response.text.split("\n")
+    cereals = [row for row in csv.DictReader(lines)]
+    get_dagster_logger().info(f"Found {len(cereals)} cereals")
+    
+@job
+def hello_cereal_job():
+    hello_cereal()
+    
+        
